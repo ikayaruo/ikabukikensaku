85a6487edd22d486be719758bc03992921ab10f5
ikayaruo <ikayaruo@gmail.com>
2018-08-03 17:29:12 +0900

    fix: wiki への link

--- src/components/links.vue
+++ src/components/links.vue
@@ -3,7 +3,7 @@
     <h2>リンク</h2>
     <p>splatoon2のデータ, 画像, フォントを利用させて頂きました。ありがとうございます。</p>
     <ul class="list-group text-left">
-      <li class="list-group-item"><a href="http://wikiwiki.jp/splatoon2ch/" target="_blank">スプラトゥーン(Splatoon) for 2ch Wiki</a></li>
+      <li class="list-group-item"><a href="https://wikiwiki.jp/splatoon2mix/" target="_blank">スプラトゥーン(Splatoon) for 2ch Wiki</a></li>
       <li class="list-group-item"><a href="https://splatoonwiki.org/wiki/Main_Page" target="_blank">Inkipedia</a></li>
       <li class="list-group-item"><a href="http://aramugi.com/?page_id=807" target="_blank">フリーフォント「イカモドキ」</a></li>
     </ul>
